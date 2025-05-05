c1a4c6028c0611ae10ee3ca77288e55db164aab8
Eiichi Sato <sato.eiichi@gmail.com>
2025-05-06 08:51:26 +0900

    pick: add pick/1 function

--- jackson-jq/src/main/java/net/thisptr/jackson/jq/Versions.java
+++ jackson-jq/src/main/java/net/thisptr/jackson/jq/Versions.java
@@ -9,8 +9,10 @@ public class Versions {
 
 	public static final Version JQ_1_6 = new Version(1, 6);
 
+	public static final Version JQ_1_7 = new Version(1, 7);
+
 	public static List<Version> versions() {
-		return Arrays.asList(JQ_1_5, JQ_1_6);
+		return Arrays.asList(JQ_1_5, JQ_1_6, JQ_1_7);
 	}
 
 	private Versions() {}
